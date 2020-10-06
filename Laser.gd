274e2e6e2668aada6ea75db24e2adecd9b3f9d9f
Michael <michael.john.davis.1@gmail.com>
2020-10-06 20:59:47 +1300

    More laser work
    - Fixes lasers rotating with the player (needed to be instanced as child of main not player)
    - Adds lights to the lasers

--- Laser.gd
+++ Laser.gd
@@ -1,12 +1,13 @@
 extends Area2D
 
 var laser_direction
-var laser_speed = 300
+var laser_speed = 500
 var laser_velocity
 
 func _ready():
-	laser_direction = get_parent().rotation - PI/2
+	self.transform = get_parent().get_node("Player").transform
+	laser_direction = rotation - PI/2
+	laser_velocity = Vector2(laser_speed, 0).rotated(laser_direction)
 
 func _process(delta):
-	laser_velocity = Vector2(laser_speed, 0).rotated(laser_direction)
 	global_position += laser_velocity * delta
--- Player.gd
+++ Player.gd
@@ -33,4 +33,4 @@ func _process(delta):
 	
 	if Input.is_action_pressed("ui_select"):
 		var laser = Laser.instance()
-		add_child(laser)
+		get_parent().add_child(laser)
